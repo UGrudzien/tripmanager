1468430a50cebbb64399fa38d7b86734f13b0e03
student3 <student3@pcoip.ki.agh.edu.pl>
2018-03-17 15:52:02 +0100

    third failure  test

--- src/test/java/pl/edu/agh/mwo/Photo.java
+++ src/test/java/pl/edu/agh/mwo/Photo.java
@@ -1,13 +1,15 @@
 package pl.edu.agh.mwo;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 public class Photo {
 	@Test
 	public void photoCount(){
-		Photo myFoto = new Photo();
-	myFoto.photoCount();
-		
+//		Photo myFoto = new Photo();
+//		myFoto.photoCount();
+		assertTrue(false);
 			
 		}
 	}
