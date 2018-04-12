262677e279e93a199932b8266fa7d0ec8f97f84c
LENOVO-PC\Urszula <ugrudzien12@gmail.com>
2018-04-12 22:10:07 +0200

    adding get iset method to Trip

--- src/test/java/pl/edu/agh/mwo/Trip.java
+++ src/test/java/pl/edu/agh/mwo/Trip.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class Trip {
+	private String name;
+	private String description;
+	
+	public void getName(){
+		name = this.name;
+	}
+	public String setName(String name){
+		return name;
+	}
 
 	Collection<Photo> photos = new ArrayList<Photo>();
 	
